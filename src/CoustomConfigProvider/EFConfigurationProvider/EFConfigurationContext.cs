0048e01601bb217270eabfb29ce99aeb76d981d4
kyle <kylerlimy@gmail.com>
2019-01-19 22:03:54 +0800

    Add Configuration Examples

---
+++ src/CoustomConfigProvider/EFConfigurationProvider/EFConfigurationContext.cs
@@ -0,0 +1,18 @@
+﻿using CoustomConfigProvider.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoustomConfigProvider.EFConfigurationProvider
+{
+    public class EFConfigurationContext : DbContext
+    {
+        public EFConfigurationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<EFConfigurationValue> Values { get; set; }
+    }
+}
---
+++ src/CoustomConfigProvider/Extensions/EntityFrameworkExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CoustomConfigProvider.EFConfigurationProvider;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoustomConfigProvider.Extensions
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static IConfigurationBuilder AddEFConfiguration(
+       this IConfigurationBuilder builder,
+       Action<DbContextOptionsBuilder> optionsAction)
+        {
+            return builder.Add(new EFConfigurationSource(optionsAction));
+        }
+
+    }
+}
